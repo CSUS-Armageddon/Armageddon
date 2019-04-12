9655ec1cff3df3912b31e395f2b3349d6764d215
Jason Sipula <alupis1@gmail.com>
2019-04-11 21:38:15 -0700

    simplify compile script

--- src/compile.bat
+++ src/compile.bat
@@ -1,19 +1,9 @@
-del a3\*.class
-del a3\network\api\*.class
-del a3\network\api\messages\*.class
-del a3\network\api\messages\impl\*.class
-del a3\network\client\*.class
-del a3\network\logging\*.class
-del a3\network\server\*.class
-del a3\network\server\impl\*.class
-del myGameEngine\asset\*.class
-del myGameEngine\controller\*.class
-del myGameEngine\controller\controls\*.class
-del myGameEngine\mesh\*.class
+del /S a3\*.class
+del /S myGameEngine\*.class
 
 javac a3\*.java
 javac a3\network\server\*.java
 
-xcopy /e /v /y ..\assets assets
+echo d | xcopy /e /v /y ..\assets assets
 
 pause
