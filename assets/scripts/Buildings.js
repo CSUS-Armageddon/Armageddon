e4d14a421b0c0259d79cf26434c348f90f0a4736
Jason Sipula <alupis1@gmail.com>
2019-04-15 23:19:58 -0700

    generate a full city block of buildings

--- assets/scripts/Buildings.js
+++ assets/scripts/Buildings.js
@@ -7,18 +7,30 @@ var JavaPackages = new JavaImporter(
 
 with (JavaPackages) {
 	function configureBuildings(sm) {
-		var MAX_BUILDINGS = 10;
+		var MAX_BUILDINGS = 9;
+		var SEPARATION = 112.5;
 		var x = 118.53;
 		var y = 0;
-		var z = 137.70;
-		for (var i = 0; i < MAX_BUILDINGS; i++) {
-			var buildingE = sm.createEntity("buildingE_" + i, "skyscrapper_1.obj");
+		var z = 140.70;
+		erectBuildings(sm,	MAX_BUILDINGS,		x,									z,									SEPARATION,	true,	"_1");
+		erectBuildings(sm,	MAX_BUILDINGS,		x,									z + SEPARATION,						SEPARATION, false,	"_2");
+		erectBuildings(sm,	MAX_BUILDINGS,		x,									z + (SEPARATION * MAX_BUILDINGS),	SEPARATION, true,	"_3");
+		erectBuildings(sm,	MAX_BUILDINGS + 1,	x + (SEPARATION * MAX_BUILDINGS),	z,									SEPARATION,	false,	"_4");
+	}
+	
+	function erectBuildings(sm, num, x, z, mod, isX, ident) {
+		for (var i = 0; i < num; i++) {
+			var buildingE = sm.createEntity("buildingE_" + i + ident, "skyscrapper_1.obj");
 			buildingE.setPrimitive(Primitive.TRIANGLES);
-			buildingN = sm.getRootSceneNode().createChildSceneNode("buildingN_" + i);
-			buildingN.setLocalPosition(x, y, z);
+			buildingN = sm.getRootSceneNode().createChildSceneNode("buildingN_" + i + ident);
+			buildingN.setLocalPosition(x, 0.0, z);
 			buildingN.scale(50, 50, 50);
 			buildingN.attachObject(buildingE);
-			x = x + 110.0;
+			if (isX) {
+				x = x + mod;
+			} else {
+				z = z + mod;
+			}
 		}
 	}
 }
