c8c81e5349c2bb188c15a7a090b4f8a5839c6a6a
Jason Sipula <alupis1@gmail.com>
2019-04-23 00:35:06 -0700

    Place avatars on spacebar press

--- src/a3/editor/MyGameEditor.java
+++ src/a3/editor/MyGameEditor.java
@@ -1,9 +1,12 @@
 package a3.editor;
 
+import java.io.IOException;
+
 import a3.MyGame;
 import a3.avatar.Avatar;
 import a3.avatar.Avatars;
 import a3.editor.controller.controls.CycleAvatarAction;
+import a3.editor.controller.controls.PlaceAvatarAction;
 import a3.editor.controller.controls.ScaleAvatarAction;
 import a3.editor.controller.controls.VerticalAvatarAction;
 import net.java.games.input.Controller;
@@ -13,6 +16,9 @@ import ray.input.InputManager.INPUT_ACTION_TYPE;
 import ray.rage.scene.SceneManager;
 
 public class MyGameEditor extends MyGame {
+	
+	public static String SCENE_OBJECTS_NODE_GROUP = "SCENE_OBJECTS";
+	private int objectCount = 0;
 	
 	public static void main(String ... args) {
 		new MyGameEditor(null, 6868, false, Avatars.MECH.getAvatar()).init();
@@ -36,12 +42,19 @@ public class MyGameEditor extends MyGame {
 	}
 	
 	@Override
+	protected void setupObjects(SceneManager sm) throws IOException {
+		super.setupObjects(sm);
+		sm.getRootSceneNode().createChildSceneNode(MyGameEditor.SCENE_OBJECTS_NODE_GROUP);
+	}
+	
+	@Override
 	protected void setupInputs(SceneManager sm) {
 		super.setupInputs(sm);
 		
 		final CycleAvatarAction cycleAvatarAction = new CycleAvatarAction(sm, this);
 		final ScaleAvatarAction scaleAvatarAction = new ScaleAvatarAction(sm);
 		final VerticalAvatarAction verticalAvatarAction = new VerticalAvatarAction(sm, this);
+		final PlaceAvatarAction placeAvatarAction = new PlaceAvatarAction(sm, this);
 		
 		// hack to change avatars to first placeableAvatar
 		cycleAvatarAction.performAction(0, null);
@@ -59,8 +72,19 @@ public class MyGameEditor extends MyGame {
 		    	// up and down
 		    	im.associateAction(c, Key.I, verticalAvatarAction, INPUT_ACTION_TYPE.REPEAT_WHILE_DOWN);
 		    	im.associateAction(c, Key.K, verticalAvatarAction, INPUT_ACTION_TYPE.REPEAT_WHILE_DOWN);
+		    	
+		    	// place avatar
+		    	im.associateAction(c, Key.SPACE, placeAvatarAction, INPUT_ACTION_TYPE.ON_PRESS_ONLY);
 			}
 		}
+	}
+	
+	public void incrementObjectCount() {
+		this.objectCount++;
+	}
+	
+	public int getObjectCount() {
+		return this.objectCount;
 	}
 
 }
---
+++ src/a3/editor/controller/controls/PlaceAvatarAction.java
@@ -0,0 +1,53 @@
+package a3.editor.controller.controls;
+
+import a3.MyGame;
+import a3.editor.MyGameEditor;
+import net.java.games.input.Event;
+import ray.input.action.Action;
+import ray.rage.rendersystem.Renderable.Primitive;
+import ray.rage.scene.Entity;
+import ray.rage.scene.SceneManager;
+import ray.rage.scene.SceneNode;
+
+public class PlaceAvatarAction implements Action {
+
+	private final SceneManager sm;
+	private final MyGameEditor editor;
+	
+	public PlaceAvatarAction(SceneManager sm, MyGameEditor editor) {
+		this.sm = sm;
+		this.editor = editor;
+		//this.playerNode = (SceneNode) sm.getRootSceneNode().getChild(MyGame.PLAYER_NODE_NAME);
+	}
+	
+	@Override
+	public void performAction(float time, Event evt) {
+		try {
+			// get object group
+			final SceneNode sceneObjectGroup = 
+					(SceneNode) sm.getRootSceneNode().getChild(MyGameEditor.SCENE_OBJECTS_NODE_GROUP);
+			
+			// new object being created
+			editor.incrementObjectCount();
+			final Entity en = 
+					sm.createEntity("Object_" + editor.getObjectCount(), editor.getAvatar().getAvatarFileName());
+			en.setPrimitive(Primitive.TRIANGLES);
+			
+			// new node for this object
+			final SceneNode newObjectNode = 
+					(SceneNode) sceneObjectGroup.createChildNode("Object_" + editor.getObjectCount() + "_Node");
+			
+			// attach object to node
+			newObjectNode.attachObject(en);
+			
+			// clone attributes of current player node
+			final SceneNode playerNode = (SceneNode) sm.getRootSceneNode().getChild(MyGame.PLAYER_NODE_NAME);
+			newObjectNode.setLocalPosition(playerNode.getLocalPosition());
+			newObjectNode.setLocalRotation(playerNode.getLocalRotation());
+			newObjectNode.scale(playerNode.getWorldScale());
+		} catch (Exception e) {
+			e.printStackTrace(); // don't crash no matter what... please...
+		}
+	}
+	
+}
