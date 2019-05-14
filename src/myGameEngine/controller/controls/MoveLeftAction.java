85ebaaa7610f8f6801484d6066d405b626869a30
Jason Sipula <alupis1@gmail.com>
2019-05-14 14:44:08 -0700

    fix direction of straffing

--- src/myGameEngine/controller/controls/MoveLeftAction.java
+++ src/myGameEngine/controller/controls/MoveLeftAction.java
@@ -22,10 +22,10 @@ public class MoveLeftAction implements Action {
 		float movement = 0.0f;
 		final String c = evt.getComponent().getName().toLowerCase();
 		if (c.contentEquals("d")) {
-			movement = evt.getValue() * MyGame.PLAYER_SPEED;
+			movement = -1.0f * evt.getValue() * MyGame.PLAYER_SPEED;
 		}
 		else if (c.contentEquals("a")) {
-			movement = -1.0f * evt.getValue() * MyGame.PLAYER_SPEED;
+			movement = evt.getValue() * MyGame.PLAYER_SPEED;
 		}
 		else if (c.contentEquals("x axis")) {
 			if (evt.getValue() < -0.2f) {
